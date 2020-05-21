d37203e049c2b7faf444fe94ec898ddc8d80da47
egin10 <egin066@gmail.com>
2020-05-21 13:41:07 +0700

    edit fullscreen

--- app/src/main/java/id/abonsapi/splashscreen/SplashscreenActivity.java
+++ app/src/main/java/id/abonsapi/splashscreen/SplashscreenActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
@@ -22,6 +23,7 @@ public class SplashscreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splashscreen);
 
         topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
